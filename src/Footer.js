9a10a713dcd4d545e148da019964010b7955aedb
Aditya <imadityamishra19@gmail.com>
2023-08-25 14:17:14 +0530

    First Commit

---
+++ src/Footer.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export const Footer = () => {
+  return (
+    <footer>
+      Made by <a href="https://github.com/Neonative52">Aditya Mishra</a>{' '}
+    </footer>
+  );
+};
